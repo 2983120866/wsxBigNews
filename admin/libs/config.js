cb17d3714c73869d393e1c707fbb18a8d5321d5c
wang <2983120866@qq.com>
2019-10-13 17:22:42 +0800

    文字列表修改完成

---
+++ admin/libs/config.js
@@ -0,0 +1,28 @@
+//入口函数
+$(function(){
+    // 日期初始化
+    jeDate("#testico",{
+        format:"YYYY-MM-DD",
+        isinitVal: true,//默认显示当前日期
+        festival:true,
+        theme:{ bgcolor:"#00A1CB",color:"#ffffff", pnColor:"#00CCFF"},
+    });
+
+
+    // 富文本初始化
+    tinymce.init({
+        selector: '#mytextarea',
+        language: 'zh_CN',
+        directionality: 'ltl',
+        browser_spellcheck: true,
+        contextmenu: false,
+        plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table contextmenu paste imagetools wordcount",
+            "code"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code",
+
+    });
+})
